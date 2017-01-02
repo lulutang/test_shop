67cb404e700e8c634fd4911025a31dd38db52619
唐路路 <tanglulu@mysipo.com>
2017-01-02 23:05:31 +0800

    编辑员工信息，删除员工信息

--- application/admin/controller/Info.php
+++ application/admin/controller/Info.php
@@ -89,6 +89,101 @@ class Info
     }
     
     /**
+     * 查看信息
+     */
+    public function edit(){
+        if ($this->request->isAjax() && $this->request->isPost()) {
+            $data = $post =  $this->request->post();
+            $id = $data['id'];
+            unset($data['id']);
+            $list = Db::name("AdminMember")->field('id')->where(array('id'=>$id, 'status'=>0))->find();
+            if(!$list){
+            	return ajax_return_adv('数据不存在，请联系管理员核实', '');
+            }
+            
+            // 写入数据表
+            
+            
+            $data['admin_id'] = UID;
+            $data['update_time'] = time();
+            unset($data['rz_time']);
+            unset($data['corporate_name']);
+            unset($data['money']);
+            unset($data['reasons']);
+            unset($data['sposition']);
+            unset($data['zm_name']);
+             
+            unset($data['etime']);
+            unset($data['ename']);
+            unset($data['zhuanye']);
+            unset($data['type']);
+            unset($data['is_biye']);
+            unset($data['xuewei']);
+             
+            unset($data['fname']);
+            unset($data['guanxi']);
+            unset($data['fsex']);
+            unset($data['fage']);
+            unset($data['fmobile']);
+            unset($data['e_ids']);
+            unset($data['rz_ids']);
+            unset($data['f_ids']);
+            
+            $id = Db::name("AdminMember")->where(array('id'=>$id))->update($data);
+           
+            if($id){
+            	for ($i=0;$i<3;$i++){
+            		$curricum['rz_time']        = $post['rz_time'][$i];
+            		$curricum['corporate_name'] = $post['corporate_name'][$i];
+            		$curricum['money']          = $post['money'][$i];
+            		$curricum['reasons']        = $post['reasons'][$i];
+            		$curricum['zm_name']        = $post['zm_name'][$i];
+            		$curricum['sposition']       = $post['sposition'][$i];
+            		$id_c = Db::name("AdminMemberCurricum")->where(array('id' => $post['rz_ids'][$i]))->update($curricum);
+            	}
+            		
+            	for ($i=0;$i<2;$i++){
+            		$education['etime']        = $post['etime'][$i];
+            		$education['ename']        = $post['ename'][$i];
+            		$education['zhuanye']      = $post['zhuanye'][$i];
+            		$education['type']         = $post['type'][$i];
+            		$education['is_biye']      = $post['is_biye'][$i];
+            		$education['xuewei']       = $post['xuewei'][$i];
+            		$id_e = Db::name("AdminMemberEducation")->where(array('id' => $post['e_ids'][$i]))->update($education);
+            		
+            	}
+            
+            	for ($i=0;$i<2;$i++){
+            		$family['fname']        = $post['fname'][$i];
+            		$family['guanxi']       = $post['guanxi'][$i];
+            		$family['fsex']         = $post['fsex'][$i];
+            		$family['fage']         = $post['fage'][$i];
+            		$family['fmobile']      = $post['fmobile'][$i];
+            		$id_f = Db::name("AdminMemberFamily")->where(array('id' => $post['f_ids'][$i]))->update($family);
+            		
+            	}
+            	return ajax_return_adv('修改成功', 'parent');
+            }else{
+            	return ajax_return_adv('修改失败，请重试', '');
+            }
+        }else{
+            $id = $this->request->param('id');
+            $list = Db::name("AdminMember")->where(array('id' => $id))->find();
+            
+            $Curricum  = Db::name("AdminMemberCurricum")->where(array('mid' => $id))->select();
+            $Education = Db::name("AdminMemberEducation")->where(array('mid' => $id))->select();
+            $Family    = Db::name("AdminMemberFamily")->where(array('mid' => $id))->select();
+            
+            $this->view->assign('vo', $list);
+            $this->view->assign('curricum', $Curricum);
+            $this->view->assign('education', $Education);
+            $this->view->assign('family', $Family);
+            return $this->view->fetch();
+        }
+    	
+    }
+    
+    /**
      * 增加员工
      */
    public function add()
@@ -178,6 +273,31 @@ class Info
 	   	}
    }
 
-
+   public function delete()
+   {
+   	if ($this->request->isAjax() && $this->request->isPost()) {
+   		$data = $this->request->post();
+   		$list = Db::name("AdminMember")->field('id')->where(array('id' => array('in',$data['id']),'status' => 0))->find();
+   		if(!$list){
+   			return ajax_return_adv('删除异常', '');
+   		}
+   		// 更新数据表
+   		 
+   		$log['status'] = 1;
+   		$log['admin_id'] = UID;
+   		$log['update_time'] = time();
+   		$id = Db::name("AdminMember")->where(array('id' => array('in',$data['id'])))->update($log);
+   		if($id){
+   			return ajax_return_adv('删除成功', 'parent');
+   		}else{
+   			return ajax_return_adv('删除失败，请重试', '');
+   				
+   		}
+   		 
+   	}else{
+   		return $this->view->fetch();
+   		 
+   	}
+   }
    
 }
